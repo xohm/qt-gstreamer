582eb4096340a66878c41645dc8d2029029dd8d8
Diane Trout <diane@ghic.org>
2013-10-26 20:14:09 -0700

    gst_pad_get_caps_reffed was removed
    The recommendation to use gst_pad_query_caps. However that API
    call doesn't return a writable caps. If that's needed
    the suggestion is to use gst_caps_make_writeable

--- src/QGst/pad.cpp
+++ src/QGst/pad.cpp
@@ -72,7 +72,7 @@ bool Pad::unlink(const PadPtr & sink)
 
 CapsPtr Pad::caps() const
 {
-    return CapsPtr::wrap(gst_pad_get_caps_reffed(object<GstPad>()), false);
+    return CapsPtr::wrap(gst_pad_query_caps(object<GstPad>(), NULL), false);
 }
 
 CapsPtr Pad::allowedCaps() const
